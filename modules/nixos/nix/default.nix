8d08bcb71d05c35d062597010deeffd08991df4f
Ricardo Correa <r.correa.r@gmail.com>
2024-09-28 00:01:46 -0400

    Removed nvd diff hook

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -19,8 +19,6 @@
         settings = {
           allow-import-from-derivation = lib.mkDefault true;
           auto-optimise-store = lib.mkDefault true;
-          diff-hook = "${pkgs.nvd}/bin/nvd";
-          run-diff-hook = lib.mkDefault true;
         };
       };
     }
