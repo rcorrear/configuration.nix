cc06794e18eba6faac964c986338a8189ccfa8b0
Ricardo Correa <r.correa.r@gmail.com>
2024-10-05 15:04:43 -0400

    Add devenv

--- flake.nix
+++ flake.nix
@@ -2,8 +2,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
@@ -31,6 +38,10 @@
         # Allow unfree packages.
         allowUnfree = true;
       };
+
+      nixos.modules = [
+        inputs.devenv.flakeModule
+      ];
 
       # The `src` must be the root of the flake. See configuration
       # in the next section for information on how you can move your
---
+++ shells/default/default.nix
@@ -0,0 +1,31 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib,
+  # You also have access to your flake's inputs.
+  inputs,
+
+  # The namespace used for your flake, defaulting to "internal" if not set.
+  namespace,
+
+  # All other arguments come from NixPkgs. You can use `pkgs` to pull shells or helpers
+  # programmatically or you may add the named attributes as arguments here.
+  pkgs,
+  mkShell,
+  ...
+}:
+inputs.devenv.lib.mkShell {
+  inherit inputs pkgs;
+  modules = [
+    (
+      { pkgs, config, ... }:
+      {
+        pre-commit.hooks = {
+          flake-checker.enable = true;
+          nixfmt-rfc-style.enable = true;
+          statix.enable = true;
+        };
+      }
+    )
+  ];
+}
