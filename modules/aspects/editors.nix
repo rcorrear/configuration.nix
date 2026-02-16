6b83ed14ca9a0638bbfcd46af7663611852d8968
Ricardo Correa <r.correa.r@gmail.com>
2026-02-16 16:45:58 -0500

    aspects: fix darwin compatibility, remove .pre-commit-config.yaml

--- modules/aspects/editors.nix
+++ modules/aspects/editors.nix
@@ -3,13 +3,18 @@
   den.aspects.editors = {
     includes = [ den.aspects.emacs ];
 
+    darwin = _: {
+      homebrew = {
+        casks = [
+          "zed"
+        ];
+      };
+    };
+
     homeManager =
       { lib, pkgs, ... }:
       {
-        home.packages = [
-          pkgs.zed-editor
-        ]
-        ++ lib.optionals pkgs.stdenv.isLinux [
+        home.packages = lib.optionals pkgs.stdenv.isLinux [
           pkgs.jetbrains.idea-oss
           pkgs.jetbrains.rider
         ];
