5b40951869dd8add657241d3d112acefe5cdc11b
Ricardo Correa <r.correa.r@gmail.com>
2026-02-15 21:58:43 -0500

    aspects/defaults: restore den home-manager includes

--- modules/aspects/defaults.nix
+++ modules/aspects/defaults.nix
@@ -1,14 +1,10 @@
 { den, ... }:
 {
-  den.default.includes =
-    let
-      hm = ctx: (den._.home-manager ctx) // { includes = [ ]; }; # Clear HM includes to avoid default include chains conflicting with den.aspects routing.
-    in
-    [
-      hm
-      den.aspects.nixos-base
-      den.aspects.nixpkgs-config
-      den.aspects.routes
-      den.aspects.timezone
-    ];
+  den.default.includes = [
+    den._.home-manager
+    den.aspects.nixos-base
+    den.aspects.nixpkgs-config
+    den.aspects.routes
+    den.aspects.timezone
+  ];
 }
