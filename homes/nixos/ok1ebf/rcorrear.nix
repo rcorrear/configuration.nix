bc980631eed1f5db1845bc40f358b60f528230f1
Ricardo Correa <r.correa.r@gmail.com>
2026-02-16 11:54:50 -0500

    aspects/workstation: move desktop app set and linux-only tools out of ok1ebf home

--- homes/nixos/ok1ebf/rcorrear.nix
+++ homes/nixos/ok1ebf/rcorrear.nix
@@ -1,24 +1,8 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   imports = [
     ../../all/rcorrear.nix
   ];
-
-  dconf = {
-    enable = true;
-    settings."org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        caffeine.extensionUuid
-        gsconnect.extensionUuid
-        tailscale-status.extensionUuid
-        vitals.extensionUuid
-      ];
-    };
-  };
 
   home = {
     keyboard = {
@@ -39,72 +23,24 @@
 
       pkgs.atlauncher
       pkgs.celluloid
-      pkgs.desktop-file-utils
-      pkgs.evolution
-      pkgs.firefox
       pkgs.font-awesome
-      pkgs.ghostty
-      pkgs.gnome-terminal
-      pkgs.gnome-tweaks
-      pkgs.gnomeExtensions.caffeine
-      pkgs.gnomeExtensions.gsconnect
-      pkgs.gnomeExtensions.tailscale-status
-      pkgs.gnomeExtensions.vitals
       pkgs.gwe
       pkgs.lutris
-      pkgs.maestral
       pkgs.mangohud
       pkgs.moonlight-qt
-      pkgs.mpg123
-      pkgs.mprime
-      pkgs.mtr-gui
-      pkgs.nfs-utils
-      pkgs.ntfs3g
       pkgs.nvtopPackages.nvidia
-      pkgs.pciutils
       pkgs.piper
       pkgs.plexamp
       pkgs.prismlauncher
-      pkgs.psmisc
-      pkgs.remmina
-      pkgs.simple-scan
-      pkgs.sshpass
       pkgs.sunshine
       pkgs.umu-launcher
-      pkgs.usbutils
-      pkgs.virtiofsd
       pkgs.vorbis-tools
-      pkgs.vulkan-tools
-      pkgs.wireguard-tools
-      pkgs.xclip
-      pkgs.xfsprogs
-      pkgs.yubioath-flutter
-      pkgs.zstd
     ];
 
     stateVersion = "21.05";
   };
 
   programs = {
-    foot = {
-      enable = true;
-      server.enable = true;
-      settings = {
-        csd = {
-          border-width = 2;
-          border-color = "ff404040";
-        };
-
-        main = {
-          term = "xterm-256color";
-        };
-
-        mouse = {
-          hide-when-typing = "yes";
-        };
-      };
-    };
-
     obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [ obs-pipewire-audio-capture ];
