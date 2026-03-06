c5093ce7e0757dd6932155358d8d85b7e15e868c
Ricardo Correa <r.correa.r@gmail.com>
2026-03-06 12:25:07 -0500

    feat(devshell): add treefmt formatter and lint command via nix fmt

---
+++ modules/treefmt.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  lib,
+  self,
+  ...
+}:
+{
+  flake-file.inputs.treefmt-nix = {
+    url = "github:numtide/treefmt-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  imports = lib.optionals (inputs ? treefmt-nix) [ inputs.treefmt-nix.flakeModule ];
+}
+// lib.optionalAttrs (inputs ? treefmt-nix) {
+  perSystem =
+    { config, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs = {
+          deadnix.enable = true;
+          nixfmt.enable = true;
+          shfmt.enable = true;
+          yamllint.enable = true;
+          yamlfmt.enable = true;
+        };
+      };
+
+      checks.formatting = config.treefmt.build.check self;
+      formatter = config.treefmt.build.wrapper;
+    };
+}
