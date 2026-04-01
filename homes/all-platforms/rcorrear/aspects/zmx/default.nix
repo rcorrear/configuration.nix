86bb1f5ef302f1b229d40391b5f0d80bef00e561
Ricardo Correa <r.correa.r@gmail.com>
2026-04-01 11:31:01 -0400

    refactor(zmx): extract zmx to an aspect

---
+++ homes/all-platforms/rcorrear/aspects/zmx/default.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  ...
+}:
+let
+  zmxFlake = builtins.getFlake "github:neurosnap/zmx/v0.4.2";
+in
+{
+  config = {
+    home.packages = [
+      zmxFlake.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+
+    programs.starship.settings = {
+      format = "\\${env_var.ZMX_SESSION}\\\n...\n";
+
+      env_var.ZMX_SESSION = {
+        symbol = " ";
+        format = "[$symbol$env_value]($style) ";
+        description = "zmx session name";
+        style = "bold magenta";
+      };
+    };
+
+    programs.ssh.matchBlocks."z.*" = {
+      controlMaster = "auto";
+      controlPersist = "10m";
+      hostname = "%h";
+      proxyCommand = "sh -c 'hn=\${1#z.}; exec nc \"$hn\" %p' sh %n";
+      extraOptions = {
+        ConnectTimeout = "5";
+        RemoteCommand = "zmx attach %k";
+        RequestTTY = "yes";
+      };
+    };
+  };
+}
