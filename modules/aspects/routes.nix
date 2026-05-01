654736f237cdc85a5596cac82f0efe008a16a4fa
Ricardo Correa <r.correa.r@gmail.com>
2026-05-01 10:39:21 -0400

    feat(routes): support aspect attr refs in mutual routing

--- modules/aspects/routes.nix
+++ modules/aspects/routes.nix
@@ -3,7 +3,33 @@
   den.aspects.routes =
     let
       inherit (den.lib) parametric;
-      mutual = from: to: den.aspects.${from.aspect}._.${to.aspect} or { includes = [ ]; };
+      aspectRef =
+        entity:
+        let
+          aspect = entity.aspect or null;
+        in
+        if builtins.isString aspect then den.aspects.${aspect} or null else aspect;
+      aspectName =
+        entity:
+        let
+          aspect = entity.aspect or null;
+        in
+        if builtins.isAttrs aspect && aspect ? name && builtins.isString aspect.name then
+          aspect.name
+        else if builtins.isString aspect then
+          aspect
+        else
+          entity.name or null;
+      mutual =
+        from: to:
+        let
+          toName = aspectName to;
+          fromAspect = aspectRef from;
+        in
+        if fromAspect == null || toName == null then
+          { includes = [ ]; }
+        else
+          fromAspect._.${toName} or { includes = [ ]; };
       routes =
         {
           host,
